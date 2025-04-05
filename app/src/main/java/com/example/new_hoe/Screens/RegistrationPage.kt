178b80339b6a0761a20e4490049eb021b1840df3
Abhi95081 <abhirou7@gmail.com>
2025-04-05 22:01:06 +0530

    Registration Commit

--- app/src/main/java/com/example/new_hoe/Screens/RegistrationPage.kt
+++ app/src/main/java/com/example/new_hoe/Screens/RegistrationPage.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
@@ -14,6 +15,7 @@ import androidx.navigation.NavController
 
 @Composable
 fun RegistrationPage(navController: NavController) {
+
     var name by remember { mutableStateOf("") }
     var mobile by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
@@ -22,55 +24,67 @@ fun RegistrationPage(navController: NavController) {
     var context = LocalContext.current
 
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "Registration Page", style = MaterialTheme.typography.headlineMedium)
-        Spacer(modifier = Modifier.height(16.dp))
-
-        OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Name") },
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
+        ) {
+            Text(text = "Registration Page", style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
-            value = mobile,
-            onValueChange = { mobile = it },
-            label = { Text("Mobile No.") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
 
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = mobile,
+                onValueChange = { mobile = it },
+                label = { Text("Mobile No.") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
 
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
 
-        Button(onClick = {
-            // Handle regular login
-            Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
-            // Navigate to the Bottom Navigation Bar
-            navController.navigate("bottom_nav") {
-                // Optional: Clear the back stack
-                popUpTo("register") { inclusive = true }
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                // Handle regular login
+                Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
+                // Navigate to the Bottom Navigation Bar
+                navController.navigate("bottom_nav") {
+                    // Optional: Clear the back stack
+                    popUpTo("register") { inclusive = true }
+                }
+            },
+                modifier = Modifier.fillMaxWidth())
+            {
+                Text(text = "Register")
             }
-        }, modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Register")
         }
     }
 }
