83455568b70d47e8834d6ff567f0d979c7c6c755
Abhi95081 <abhirou7@gmail.com>
2025-04-12 21:37:46 +0530

    Register animation Commit

--- app/src/main/java/com/example/new_hoe/NavigationBar/Navgraph.kt
+++ app/src/main/java/com/example/new_hoe/NavigationBar/Navgraph.kt
@@ -3,8 +3,6 @@ package com.example.new_hoe.NavigationBar
 
 import RegistrationPage
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
